1ceeb193c9fdd520caa72a63882ed594c883ae01
sergiolozav <sergiolozav@gmail.com>
2024-01-16 16:37:35 -0400

    refactor: Borrado archivo sin uso

--- src/schemas/usuario_schemas.ts
+++
@@ -1,8 +0,0 @@
-import { Type } from '@sinclair/typebox';
-
-export class UsuarioSchemas {
-  static crearUsuario = Type.Object({
-    name: Type.String(),
-    email: Type.String({ format: 'email' }),
-  });
-}
